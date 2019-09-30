752d7a358b11511ea05926d29a8ec8797106be80
Jack Scott <jack.rob.scott@gmail.com>
2019-09-30 15:46:13 +0800

    added patch

--- src/Store.ts
+++ src/Store.ts
@@ -5,48 +5,74 @@ export class Store<T> {
   private initial: T
   private dispatcher: Dispatcher<T>
   private local?: string
-
+  /**
+   * Load the state from local host
+   * and initialise the store.
+   */
   constructor(initial: T, key?: string) {
-    this.value = initial
-    this.initial = initial
-    this.dispatcher = new Dispatcher<T>()
     this.local = key
+    this.initial = initial
+    this.value = this.load() || initial
+    this.dispatcher = new Dispatcher<T>()
+  }
+  /**
+   * Set the stores state.
+   */
+  public change(data: T): void {
+    this.value = data
+    this.persist(this.value)
+    this.dispatcher.dispatch(this.value)
+  }
+  /**
+   * Set the stores state using the previous state.
+   */
+  public patch(data: (current: T) => T): void {
+    this.value = data(this.value)
+    this.persist(this.value)
+    this.dispatcher.dispatch(this.value)
+  }
+  /**
+   * Set the store back to the defaults.
+   */
+  public reset(): void {
+    this.value = this.initial
+    this.persist(this.value)
+    this.dispatcher.dispatch(this.value)
+  }
+  /**
+   * Listen for changes to the store.
+   */
+  public listen(listener: IDispatcherListener<T>): () => void {
+    return this.dispatcher.listen(listener)
+  }
+  /**
+   * The current state of the store.
+   */
+  public get state(): T {
+    return this.value
+  }
+  /**
+   * Save data to the local storage.
+   */
+  private persist(data: T) {
     if (this.local) {
       try {
-        const data = localStorage.getItem(this.local)
-        this.value = data ? JSON.parse(data) : this.value
+        const update = JSON.stringify(data)
+        localStorage.setItem(this.local, update)
       } catch (e) {
         console.error(e)
         localStorage.removeItem(this.local)
       }
     }
   }
-
-  public change(data: T): void {
-    this.value = data
-    this.persist(this.value)
-    this.dispatcher.dispatch(this.value)
-  }
-
-  public reset(): void {
-    this.value = this.initial
-    this.persist(this.value)
-    this.dispatcher.dispatch(this.value)
-  }
-
-  public listen(listener: IDispatcherListener<T>): () => void {
-    return this.dispatcher.listen(listener)
-  }
-
-  public get state(): T {
-    return this.value
-  }
-
-  private persist(data: T) {
+  /**
+   * Load the data from the local storage.
+   */
+  private load() {
     if (this.local) {
       try {
-        const update = JSON.stringify(data)
-        localStorage.setItem(this.local, update)
+        const data = localStorage.getItem(this.local)
+        return data && JSON.parse(data)
       } catch (e) {
         console.error(e)
         localStorage.removeItem(this.local)
