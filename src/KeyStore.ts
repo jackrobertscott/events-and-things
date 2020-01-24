9412c111b4d897c239e5a3012594e111826bbdd7
Jack Scott <jack.rob.scott@gmail.com>
2020-01-24 16:33:37 +0800

    added cookie support to key store

--- src/KeyStore.ts
+++ src/KeyStore.ts
@@ -1,11 +1,17 @@
 import { Store } from './Store'
+import * as Cookies from 'js-cookie'
 
 export class KeyStore<T extends { [key: string]: any }> extends Store<T> {
   /**
    * Extend the generic value as object.
    */
-  constructor(initial: T, key?: string) {
-    super(initial, key)
+  constructor(
+    initial: T,
+    key?: string,
+    cookies: boolean = false,
+    options?: Cookies.CookieAttributes
+  ) {
+    super(initial, key, cookies, options)
   }
   /**
    * Add an update method which patches rather than sets.
